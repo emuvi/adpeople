19d44acaf513c84016c0ecb10c66321ef2ca0f5d
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-01 19:11:50 -0300

    adds the people subgroup register

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { AdMenuItem, AdModules, menuStartUp } from "adcommon";
 import { AdNation } from "./ad-nation";
 import { AdPeopleGroup } from "./ad-people-group";
+import { AdPeopleSubGroup } from "./ad-people-subgroup";
 import { AdRegion } from "./ad-region";
 
 const items: AdMenuItem[] = [
   { module: AdModules.REGION, register: AdRegion },
   { module: AdModules.NATION, register: AdNation },
   { module: AdModules.PEOPLE_GROUP, register: AdPeopleGroup },
+  { module: AdModules.PEOPLE_SUBGROUP, register: AdPeopleSubGroup },
 ];
 
 menuStartUp(items).style.putAsBody();
