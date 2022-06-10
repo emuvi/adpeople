1e614736c04fb4afec98bf0dfe7b9e0244f90b00
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-10 10:37:05 -0300

    adds business register

---
+++ src/ad-business.ts
@@ -0,0 +1,61 @@
+import { AdExpect, AdField, AdModule, AdRegBase, AdRegister, AdRegistry } from "adcommon";
+import { QinComboSet, QinMutants, QinStringSet, QinTool } from "qinpel-cps";
+
+const base = QinTool.qinpel.chief.loadConfig(QinTool.qinpel.our.names.QinBaseSelected);
+
+export const registry: AdRegistry = { base, name: "negocios" };
+
+export const register: AdRegBase = {
+  registry,
+};
+
+export class AdBusiness extends AdRegister {
+  public constructor(module: AdModule, expect: AdExpect) {
+    super(module, expect, register);
+    this.addField(
+      new AdField({
+        key: true,
+        name: "codigo",
+        title: "Código",
+        kind: QinMutants.STRING,
+        options: {
+          maxLength: 4,
+        } as QinStringSet,
+      })
+    );
+    this.addField(
+      new AdField({
+        name: "ativo",
+        title: "Ativo",
+        kind: QinMutants.COMBO,
+        options: {
+          items: [
+            {
+              title: "",
+              value: "",
+            },
+            {
+              title: "Sim",
+              value: "S",
+            },
+            {
+              title: "Não",
+              value: "N",
+            },
+          ],
+        } as QinComboSet,
+      })
+    );
+    this.addField(
+      new AdField({
+        name: "nome",
+        title: "Nome",
+        kind: QinMutants.STRING,
+        options: {
+          maxLength: 60,
+        } as QinStringSet,
+      })
+    );
+    this.prepare();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { AdMenuItem, AdModules, menuStartUp } from "adcommon";
+import { AdBusiness } from "./ad-business";
 import { AdNation } from "./ad-nation";
 import { AdPeopleGroup } from "./ad-people-group";
 import { AdPeopleSubGroup } from "./ad-people-subgroup";
 import { AdRegion } from "./ad-region";
 
 const items: AdMenuItem[] = [
+  { module: AdModules.BUSINESS, register: AdBusiness },
   { module: AdModules.REGION, register: AdRegion },
   { module: AdModules.NATION, register: AdNation },
   { module: AdModules.PEOPLE_GROUP, register: AdPeopleGroup },
