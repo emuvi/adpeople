334e7a742550522682b1ac2a3bad11ab887cab8e
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-14 14:31:35 -0300

    adds state and city registers

---
+++ src/ad-state.ts
@@ -0,0 +1,108 @@
+import {
+  AdExpect,
+  AdField,
+  AdFilter,
+  AdModule,
+  AdModules,
+  AdRegBase,
+  AdRegister,
+  AdRegistry,
+} from "adcommon";
+import { QinComboSet, QinMutants, QinStringSet, QinTool } from "qinpel-cps";
+import { registry as nation_regy } from "./ad-nation";
+
+const base = QinTool.qinpel.chief.loadConfig(QinTool.qinpel.our.names.QinBaseSelected);
+
+const registry: AdRegistry = {
+  base,
+  name: "estados",
+};
+
+const register: AdRegBase = {
+  registry,
+  joins: [
+    {
+      module: AdModules.NATION,
+      registry: nation_regy,
+      alias: "nation",
+      filters: [
+        new AdFilter({
+          linked: { name: "pais", with: "codigo" },
+        }),
+      ],
+    },
+  ],
+};
+
+export class AdState extends AdRegister {
+  public constructor(module: AdModule, expect: AdExpect) {
+    super(module, expect, register);
+    this.addField(
+      new AdField({
+        key: true,
+        name: "pais",
+        title: "País - Cód.",
+        kind: QinMutants.STRING,
+        options: {
+          maxLength: 4,
+        } as QinStringSet,
+      })
+    );
+    this.addField(
+      new AdField({
+        key: true,
+        name: "nation.nome",
+        title: "País - Nome",
+        kind: QinMutants.STRING,
+        options: {
+          maxLength: 60,
+        } as QinStringSet,
+      })
+    );
+    this.addField(
+      new AdField({
+        key: true,
+        name: "codigo",
+        title: "Código",
+        kind: QinMutants.STRING,
+        options: {
+          maxLength: 4,
+        } as QinStringSet,
+      })
+    );
+    this.addField(
+      new AdField({
+        name: "ativo",
+        title: "Ativo",
+        kind: QinMutants.COMBO,
+        options: {
+          items: [
+            {
+              title: "",
+              value: "",
+            },
+            {
+              title: "Sim",
+              value: "S",
+            },
+            {
+              title: "Não",
+              value: "N",
+            },
+          ],
+        } as QinComboSet,
+      })
+    );
+    this.addField(
+      new AdField({
+        name: "nome",
+        title: "Nome",
+        kind: QinMutants.STRING,
+        options: {
+          maxLength: 60,
+        } as QinStringSet,
+      })
+    );
+    this.prepare();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { AdMenuItem, AdModules, menuStartUp } from "adcommon";
 import { AdBusiness } from "./ad-business";
+import { AdCity } from "./ad-city";
 import { AdNation } from "./ad-nation";
 import { AdPeopleGroup } from "./ad-people-group";
 import { AdPeopleSubGroup } from "./ad-people-subgroup";
 import { AdRegion } from "./ad-region";
+import { AdState } from "./ad-state";
 
 const items: AdMenuItem[] = [
   { module: AdModules.BUSINESS, register: AdBusiness },
   { module: AdModules.REGION, register: AdRegion },
   { module: AdModules.NATION, register: AdNation },
+  { module: AdModules.STATE, register: AdState },
+  { module: AdModules.CITY, register: AdCity },
   { module: AdModules.PEOPLE_GROUP, register: AdPeopleGroup },
   { module: AdModules.PEOPLE_SUBGROUP, register: AdPeopleSubGroup },
 ];
