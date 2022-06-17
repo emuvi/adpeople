86fd8a5d8ff6589745db7860e8652a837fb7d264
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-17 16:02:27 -0300

    adds some fields of AdPeople

--- src/ad-people.ts
+++ src/ad-people.ts
@@ -15,10 +15,34 @@ export class AdPeople extends AdRegister {
     this.addTab("Principal");
     this.addField(AdTools.newAdFieldString("codigo", "Código", 8).putKey());
     this.addField(AdTools.newAdFieldAtivo());
-    this.addField(AdTools.newAdFieldString("nome", "Nome", 80));
-    this.addTab("Outros");
-    this.addField(AdTools.newAdFieldString("fantasia", "Fantasia", 60));
     this.addField(AdTools.newAdFieldBoolean("potencial", "Potencial"));
+    this.addField(AdTools.newAdFieldBoolean("cliente", "Cliente"));
+    this.addField(AdTools.newAdFieldBoolean("fornecedor", "Fornecedor"));
+    this.addField(AdTools.newAdFieldBoolean("transportadora", "Transportadora"));
+    this.addField(AdTools.newAdFieldBoolean("colaborador", "Colaborador"));
+    this.addField(AdTools.newAdFieldBoolean("consultor", "Consultor"));
+    this.addField(AdTools.newAdFieldString("nome", "Nome", 80));
+    this.addField(AdTools.newAdFieldString("fantasia", "Fantasia", 60));
+    this.addField(AdTools.newAdFieldCombo("natureza", "Natureza", naturezaFieldItems));
+    this.addField(AdTools.newAdFieldString("cnpjcpf", "CNPJ/CPF", 20));
+    this.addField(AdTools.newAdFieldString("insestadual", "Ins. Estadual", 20));
+    this.addTab("Contato");
+    this.addField(AdTools.newAdFieldString("contato", "Contato", 45));
     this.prepare();
   }
 }
+
+const naturezaFieldItems = [
+  {
+    title: "",
+    value: "",
+  },
+  {
+    title: "Física",
+    value: "F",
+  },
+  {
+    title: "Jurídica",
+    value: "J",
+  },
+];
